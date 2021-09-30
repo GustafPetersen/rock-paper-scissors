f0f37c647323148403180116ca1b675c043e3aac
Gustaf Petersen <gustaf.petersen@gmail.com>
2021-09-30 07:55:05 +0200

    updating several js, stylesheet and html

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,8 @@ which corresponds to either rock paper or scissors.
 
 */
 const gameState = {
-    scoreComp: 0,
-    scoreUser: 0,
+    SCORECOMP: 0,
+    SCOREUSER: 0
     // scoreTotal: scoreUser + scoreComp
 }
 
@@ -16,12 +16,18 @@ const gameOutcome = {
 	TIE: "tie",
 }
 
+const userImageSelect = {
+    ROCK: "rock",
+    PAPER: "paper",
+    SCISSORS: "scissors"
+}
+
 
 function computerPlay() {
     // creating an array that corresponds to computer choices
     const unitArray = ["rock", "paper", "scissors"];
     // creating random function to choose between 0, 1 and 2.
-    let computerChoice = Math.floor(Math.random()*unitArray.length);
+    const computerChoice = Math.floor(Math.random()*unitArray.length);
     /* return the index of unitArray of the integers ranging from 0-2 
     from the random computerChoice variable.
      */
@@ -31,14 +37,14 @@ function computerPlay() {
 
 
 // creating a function where the player inputs their choice and calls the computerPla
-function gamePlay() {
-    const userSelect = prompt("Select Rock, Paper or Scissors: ").toLowerCase()
+function gamePlay(userInput) {
+    const userSelect = userInput
+    console.log("You chose : " + userSelect)
     // const unitArray = ["rock", "paper", "scissors"];
     // userSelect = unitArray[Math.floor(Math.random()*unitArray.length)];
     const computerSelect = computerPlay();
-
     console.log("The computer chose: " + computerSelect)
-    console.log("You chose : " + userSelect)
+
     /*
     If, else if, and else statements determining whether the user or the computer wins.
     if the computer wins its score increments with one.
@@ -46,22 +52,51 @@ function gamePlay() {
     */
     
     if (userSelect === "rock" && computerSelect === "paper") {
+        console.log(gameOutcome.COMPUTERWIN)
+        gameState.SCORECOMP += 1
+        document.getElementsByClassName("user-score").innerHTML = "Your score: " + gameState.SCOREUSER;
+        document.getElementsByClassName("computer-score").innerHTML = "Alien score: " + gameState.SCORECOMP;
+        document.getElementsByClassName("games-left").innerHTML = "Your score: " + (5 - (gameState.SCORECOMP + gameState.SCOREUSER));
         return gameOutcome.COMPUTERWIN;
     } else if (userSelect === "rock" && computerSelect === "scissors") {
+        console.log(gameOutcome.USERWIN)
+        gameState.SCOREUSER += 1
+        document.getElementsByClassName("user-score").innerHTML = "Your score: " + gameState.SCOREUSER;
+        document.getElementsByClassName("computer-score").innerHTML = "Alien score: " + gameState.SCORECOMP;
+        document.getElementsByClassName("games-left").innerHTML = "Your score: " + (5 - (gameState.SCORECOMP + gameState.SCOREUSER));
         return gameOutcome.USERWIN
     } else if(userSelect === "scissors" && computerSelect === "rock") {
+        console.log(gameOutcome.COMPUTERWIN)
+        gameState.SCOREUSER += 1
+        document.getElementsByClassName("user-score").innerHTML = "Your score: " + gameState.SCOREUSER;
+        document.getElementsByClassName("computer-score").innerHTML = "Alien score: " + gameState.SCORECOMP;
+        document.getElementsByClassName("games-left").innerHTML = "Your score: " + (5 - (gameState.SCORECOMP + gameState.SCOREUSER));
         return gameOutcome.COMPUTERWIN
     } else if (userSelect === "scissors" && computerSelect === "paper") {
+        console.log(gameOutcome.USERWIN)
+        gameState.SCOREUSER += 1
+        document.getElementsByClassName("user-score").innerHTML = "Your score: " + gameState.SCOREUSER;
+        document.getElementsByClassName("computer-score").innerHTML = "Alien score: " + gameState.SCORECOMP;
+        document.getElementsByClassName("games-left").innerHTML = "Your score: " + (5 - (gameState.SCORECOMP + gameState.SCOREUSER));
         return gameOutcome.USERWIN
     } else if (userSelect === "paper" && computerSelect === "rock") {
+        console.log(gameOutcome.USERWIN)
+        gameState.SCOREUSER += 1
+        document.getElementsByClassName("user-score").innerHTML = "Your score: " + gameState.SCOREUSER;
+        document.getElementsByClassName("computer-score").innerHTML = "Alien score: " + gameState.SCORECOMP;
+        document.getElementsByClassName("games-left").innerHTML = "Your score: " + (5 - (gameState.SCORECOMP + gameState.SCOREUSER));
         return gameOutcome.USERWIN
     } else if (userSelect === "paper" && computerSelect === "scissor") {
+        console.log(gameOutcome.COMPUTERWIN)
+        gameState.SCOREUSER += 1
+        document.getElementsByClassName("user-score").innerHTML = "Your score: " + gameState.SCOREUSER;
+        document.getElementsByClassName("computer-score").innerHTML = "Alien score: " + gameState.SCORECOMP;
+        document.getElementsByClassName("games-left").innerHTML = "Your score: " + (5 - (gameState.SCORECOMP + gameState.SCOREUSER));
         return gameOutcome.COMPUTERWIN
     } else {
+        console.log(gameOutcome.TIE)
         return gameOutcome.TIE
     }
-
-    console.log(gameState.scoreComp, gameState.scoreUser)
 }
 
 /* while(true) {
@@ -79,6 +114,12 @@ function gamePlay() {
     alert("the score is: " + "user score: " + gameState.scoreUser + " and " + "computer score: " + gameState.scoreComp)
 }
 */
+
+// initiating the gameplay function when a user clicks on one of the rock, paper, and scissor images.
+// document.getElementById("rock").onclick = gamePlay(userImageSelect.ROCK)
+// document.getElementById("paper").onclick = gamePlay(userImageSelect.PAPER)
+// document.getElementById("scissors").onclick = gamePlay(userImageSelect.SCISSORS)
+
 
 /* 
 while function to loop the gamePlay function while the score (i) is still below 5 (?) 
